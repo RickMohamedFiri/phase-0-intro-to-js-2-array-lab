1f5b90875a5cf67a01de25c3297ec7c9edd5755d
Rick Mohamed <rickmohaa@gmail.com>
2023-06-23 08:24:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat=()=>{
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield", "Ralph");
+}
+
+
+
+const destructivelyPrependCat=()=>{
+    cats.length = 0;
+    cats.unshift("Bob","Milo", "Otis", "Garfield");
+}
+
+
+const destructivelyRemoveLastCat=()=>{
+    cats.pop();
+    cats;
+}
+
+const destructivelyRemoveFirstCat=()=>{
+    cats.shift();
+    cats;
+}
+
+const appendCat=(item)=>{
+    const newCats = [item, ...cats];
+    newCats;
+}
+
+appendCat('Broom');
+
+
+
+// const prependCat=(item, cats)=>{
+//     const newCatss = [item, ...cats];
+//     return newCatss;
+// }
+
+// prependCat('Arnold', cats)
